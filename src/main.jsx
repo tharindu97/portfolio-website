4aaa249b5343f5b21afb0d60636eb0dc5d3b9741
Tharindu97 <tharindukavishna@gmail.com>
2023-09-17 00:44:17 +0530

    version 1 completed

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <App /> */}
-    <SimpleWebPage />
+    <App />
+    {/* <SimpleWebPage /> */}
   </React.StrictMode>,
 )
